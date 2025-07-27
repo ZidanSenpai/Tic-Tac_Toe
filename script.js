1dc3545c6290845639ddf2ab7b0871e174fbfeed
Zidan <zidan.senpaii@gmail.com>
2025-07-27 22:26:49 +0530

    basic scripts

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
+const boxes = document.querySelectorAll(".box");
+let turnO = false;
+let turnX = true;
 function createPlayer(){
     let score = 0;
     return {score};
 }
 function Board(){
-    const board = [
-                    [0, 0, 0],
-                    [0, 0, 0],
-                    [0, 0, 0]
-                ];
-    return {board};
+    let board = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8]
+    ];
+    const winPatterns = [
+        [0,1,2],
+        [0,3,6],
+        [0,4,8],
+        [1,4,7],
+        [2,5,8],
+        [3,4,5],
+        [6,7,8],
+        [6,4,2]
+    ];
+    return {board, winPatterns};
 }
 const gameBoard = Board();
-function playGame(){
-
-}
 const player1 = createPlayer();
-console.log(gameBoard.board[2][1]);
+boxes.forEach((box)=>{
+    box.addEventListener("click", ()=>{
+        if(turnO){
+            box.textContent = "O";
+            turnO = false;
+            turnX = true;
+        }
+        else{
+            box.textContent = "X";
+            turnX = false;
+            turnO = true;
+        }
+        box.disabled = true;
+        checkWinner();
+    })
+    
+})
+const checkWinner = () =>{
+    for(pattern in gameBoard.winPatterns){
+        console.log(pattern)
+    }
+}
