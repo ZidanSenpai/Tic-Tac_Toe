de301e4569aa79ea34422bcbec7800b075da8a8b
Zidan <zidan.senpaii@gmail.com>
2025-07-27 21:58:43 +0530

    add basic styles

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hello")
+function createPlayer(){
+    let score = 0;
+    return {score};
+}
+function Board(){
+    const board = [
+                    [0, 0, 0],
+                    [0, 0, 0],
+                    [0, 0, 0]
+                ];
+    return {board};
+}
+const gameBoard = Board();
+function playGame(){
+
+}
+const player1 = createPlayer();
+console.log(gameBoard.board[2][1]);
